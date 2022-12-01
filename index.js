8245a6ebb6619d845a5b2cd68efdc15b6c90e07b
Helena <helena@sixfeetcreative.com>
2022-12-01 20:24:12 +0100

    adding a countdown

--- index.js
+++ index.js
@@ -150,3 +150,30 @@ pokemonContainer.appendChild(pokemonEl);
 }
 
 getPokemon(194);
+
+
+const countdown = () => {
+  const countDate = new Date ('Jan 20, 2023 00:00:00').getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor((gap / day));
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+
+  document.querySelector('.day').innerText = textDay;
+  document.querySelector('.hour').innerText = textHour;
+  document.querySelector('.minute').innerText = textMinute;
+  document.querySelector('.second').innerText = textSecond;
+
+  setInterval(countdown, 1000);
+};
+
+countdown();
+
